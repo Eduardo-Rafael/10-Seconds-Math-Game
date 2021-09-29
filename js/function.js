bd932738a57838c648faa965c1b6503c7a7f0108
Eduardo Carranza <eduardocarranza0812@gmail.com>
2021-09-28 22:11:13 -0400

    Crates the GameController object

---
+++ js/function.js
@@ -0,0 +1,43 @@
+//Author: Eduardo Carranza
+
+function GameController(){
+  
+  //private properties
+
+  var firstOperand = 0;
+  var secondOperand = 0;
+  var corerctAnswer = 0;
+  var timer = 10;
+  var operators = ['+'];
+  var numberLimit = 10;
+  var score = 0;
+  var highScore = 0;
+
+
+  //public methods
+  this.generateEquation = function(){
+
+  }
+
+  this.addOperator = function(operator){
+
+  }
+
+  this.removeOperator = function(operator){
+
+  }
+
+  this.checkAnswers = function(userAnswer){
+
+  }
+
+  this.startGame = function(){
+
+  }
+
+  this.resetGame = function(){
+    
+  }
+
+
+}
